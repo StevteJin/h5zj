1af702d0eeb03c6fb5b91290b67a4fd2d900232f
dengli <dengli1001@163.com>
2019-05-28 18:42:54 +0800

    注释socket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements DoCheck, OnInit {
       this.data.token = this.data.randomString(32);
       this.data.setLocalStorage('token', this.data.token);
     }
-    this.connect();
+    // this.connect();
   }
 
   /**
@@ -106,7 +106,7 @@ export class AppComponent implements DoCheck, OnInit {
       this.data.isConnect = true;
       if (this.data.getLocalStorage('token') !== this.data.getToken()) {
         this.data.setLocalStorage('token', this.data.token);
-        this.connectWs();
+        // this.connectWs();
       }
     }
 
